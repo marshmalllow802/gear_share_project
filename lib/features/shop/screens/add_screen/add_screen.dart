4349337f611b727a0f25a92fafd8e521436a8a00
set <set@truedev.org>
2025-01-31 17:43:01 +0100

    some fixes

--- lib/features/shop/screens/add_screen/add_screen.dart
+++ lib/features/shop/screens/add_screen/add_screen.dart
@@ -67,6 +67,7 @@ class _AddScreenState extends State<AddScreen> {
   PostCategory? _selectedCategory;
   RentalPeriod? _selectedRentalPeriod;
   List<XFile>? _images;
+  bool _isLoading = false;
 
   /// Metoda do wyboru zdjęć
   Future<void> _pickImages() async {
@@ -106,6 +107,9 @@ class _AddScreenState extends State<AddScreen> {
         final uid = FirebaseAuth.instance.currentUser?.uid;
         final author = uid ?? "";
 
+        // Устанавливаем состояние загрузки
+        setState(() => _isLoading = true);
+
         // Загружаем все изображения и получаем их URL
         List<String> imagesUrls = [];
         for (var image in images) {
@@ -138,10 +142,12 @@ class _AddScreenState extends State<AddScreen> {
 
         // Переходим на главный экран
         if (!mounted) return;
+        setState(() => _isLoading = false);
         Get.to(() => const HomeScreen());
       } catch (e) {
         debugPrint("Error: $e");
         if (!mounted) return;
+        setState(() => _isLoading = false);
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content: Text('Wystąpił błąd podczas dodawania produktu.'),
@@ -153,132 +159,149 @@ class _AddScreenState extends State<AddScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: KAppBar(
-        title: Text(
-          'Dodaj ogłoszenie',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        showBackArror: true,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Pole tytułu
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(labelText: 'Tytuł *'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Proszę podać tytuł.';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: KAppBar(
+            title: Text(
+              'Dodaj ogłoszenie',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            showBackArror: true,
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: AbsorbPointer(
+              // Блокируем взаимодействие во время загрузки
+              absorbing: _isLoading,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Pole tytułu
+                    TextFormField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(labelText: 'Tytuł *'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Proszę podać tytuł.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
 
-              // Wybór kategorii
-              DropdownButtonFormField<PostCategory>(
-                value: _selectedCategory,
-                decoration: const InputDecoration(labelText: 'Kategoria *'),
-                items: PostCategory.values.map((category) {
-                  return DropdownMenuItem(
-                    value: category,
-                    child: Text(category.name), // .name odczytuje nazwę enuma
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedCategory = value;
-                  });
-                },
-                validator: (value) {
-                  if (value == null) {
-                    return 'Proszę wybrać kategorię.';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
+                    // Wybór kategorii
+                    DropdownButtonFormField<PostCategory>(
+                      value: _selectedCategory,
+                      decoration:
+                          const InputDecoration(labelText: 'Kategoria *'),
+                      items: PostCategory.values.map((category) {
+                        return DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                              category.name), // .name odczytuje nazwę enuma
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Proszę wybrać kategorię.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
 
-              // Pole opisu
-              TextFormField(
-                controller: _descriptionController,
-                maxLines: 5,
-                decoration: const InputDecoration(labelText: 'Opis'),
-              ),
-              const SizedBox(height: 16),
+                    // Pole opisu
+                    TextFormField(
+                      controller: _descriptionController,
+                      maxLines: 5,
+                      decoration: const InputDecoration(labelText: 'Opis'),
+                    ),
+                    const SizedBox(height: 16),
 
-              // Pole ceny
-              TextFormField(
-                controller: _priceController,
-                decoration: const InputDecoration(labelText: 'Cena *'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || double.tryParse(value) == null) {
-                    return 'Proszę podać poprawną cenę.';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
+                    // Pole ceny
+                    TextFormField(
+                      controller: _priceController,
+                      decoration: const InputDecoration(labelText: 'Cena *'),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || double.tryParse(value) == null) {
+                          return 'Proszę podać poprawną cenę.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
 
-              // Wybór zdjęć
-              OutlinedButton.icon(
-                onPressed: _pickImages,
-                icon: const Icon(Icons.image),
-                label: const Text('Dodaj zdjęcia'),
-              ),
-              if (_images != null && _images!.isNotEmpty)
-                Wrap(
-                  spacing: 8.0,
-                  runSpacing: 8.0,
-                  children: _images!.map((image) {
-                    return Image.file(
-                      File(image.path),
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    );
-                  }).toList(),
+                    // Wybór zdjęć
+                    OutlinedButton.icon(
+                      onPressed: _pickImages,
+                      icon: const Icon(Icons.image),
+                      label: const Text('Dodaj zdjęcia'),
+                    ),
+                    if (_images != null && _images!.isNotEmpty)
+                      Wrap(
+                        spacing: 8.0,
+                        runSpacing: 8.0,
+                        children: _images!.map((image) {
+                          return Image.file(
+                            File(image.path),
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          );
+                        }).toList(),
+                      ),
+                    const SizedBox(height: 16),
+
+                    // Wybór okresu wypożyczenia
+                    DropdownButtonFormField<RentalPeriod>(
+                      value: _selectedRentalPeriod,
+                      decoration: const InputDecoration(
+                          labelText: 'Okres wypożyczenia'),
+                      items: RentalPeriod.values.map((period) {
+                        return DropdownMenuItem(
+                          value: period,
+                          child: Text(period.name),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedRentalPeriod = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 32),
+
+                    // Przycisk do wysłania formularza
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _submitForm,
+                        child: const Text('Dodaj ogłoszenie'),
+                      ),
+                    ),
+                  ],
                 ),
-              const SizedBox(height: 16),
-
-              // Wybór okresu wypożyczenia
-              DropdownButtonFormField<RentalPeriod>(
-                value: _selectedRentalPeriod,
-                decoration:
-                    const InputDecoration(labelText: 'Okres wypożyczenia'),
-                items: RentalPeriod.values.map((period) {
-                  return DropdownMenuItem(
-                    value: period,
-                    child: Text(period.name),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedRentalPeriod = value;
-                  });
-                },
               ),
-              const SizedBox(height: 32),
-
-              // Przycisk do wysłania formularza
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Dodaj ogłoszenie'),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        if (_isLoading)
+          Container(
+            color: Colors.black26,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
     );
   }
 }
